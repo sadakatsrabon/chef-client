6355596fe22d7bf6c836c7c18c7fe40008008076
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-05-04 21:24:54 +0600

    error page style updated

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -3,20 +3,26 @@ import { Link, useRouteError } from 'react-router-dom';
 
 const Error = () => {
     const { error, status } = useRouteError();
-    console.log(error.message);
 
     return (
-        <div className='container'>
-            <div>
-            <img className='' src={"https://th.bing.com/th/id/OIP.fWdQXZCgnfStjhzbRhKOYQHaGn?pid=ImgDet&w=717&h=640&rs=1"} alt="Image" />
+        <div className='container d-flex flex-column align-items-center justify-content-center'>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 d-flex align-items-center justify-content-center">
+                        <img className='img-fluid w-25 mx-auto' src={"https://th.bing.com/th/id/OIP.fWdQXZCgnfStjhzbRhKOYQHaGn?pid=ImgDet&w=717&h=640&rs=1"} alt="Error" />
+                    </div>
+                </div>
             </div>
-            <div className=''>
-                <h3>Sorry Man!! Could Not Found The Page Thats You requested.</h3>
-                <h1 className=''>{status || 404}</h1>
-                <h2>Error:{error.message}</h2>
+            <div className='text-center mt-3'>
+                <h3>Sorry, the page you requested could not be found.</h3>
+                <h1 className='display-1'>{status || 404}</h1>
+                <h2>Error: {error.message}</h2>
             </div>
-            <div className=''>
-                <Link to="/"><button className=''>Return Home</button></Link>
+            <div className='mt-3'>
+                <Link to="/">
+                    <button className='btn btn-primary'>Return Home</button>
+                </Link>
             </div>
         </div>
     );
