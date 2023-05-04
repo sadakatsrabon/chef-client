a46ba2fc0c473f19bd155b37d7721beae3b909e6
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-05-04 21:07:25 +0600

    Home Card and Blog cleared

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,40 +1,79 @@
-import React from 'react';
+import React from "react";
+// import LazyLoad from "react-lazy-load";
+import { Link } from "react-router-dom";
 
 const Blog = () => {
+    
     return (
-        <div className='container d-flex'>
-            <h1>Answer Script</h1>
-            <h2>In this page you can see some answer of PH question.</h2>
-            <div>
-                <h3>Questions:</h3>
-                <h5>Q-1: </h5>
-                <h5>Q-2: </h5>
-                <h5>Q-3: </h5>
-                <h5>Q-4: </h5>
-                <h5>Q-5: </h5>
-            </div>
-            <div className='bg-secondary-subtle'>
-                <h3>Answer:</h3>
-                <h5>Anaswer to-1: </h5>
-                <p></p>
-                <hr />
+        <div className="container" style={{ paddingTop: "125px" }}>
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-8">
+                    <div className="card shadow-lg">
+                        <div className="card-body">
+                            <h2 className="card-title text-center">React Blog Toast</h2>
+                            <hr style={{ border: "3px solid black" }} />
+                            <h3 className="mt-4">
+                                What is the differences between Uncontrolled and Controlled
+                                components.
+                            </h3>
+                            <p>
+                                Uncontrolled components are those that manage their state
+                                internally and don't rely on React to update the UI. Controlled
+                                components, on the other hand, rely on React to manage their
+                                state and update the UI. Uncontrolled components are simpler to
+                                use, but they offer less control over the state. Controlled
+                                components provide more control over the state, but they require
+                                more work to set up and maintain.
+                            </p>
+                            <hr className="mb-3" />
+                            <h3 className="mt-4">
+                                How to validate React props using PropTypes
+                            </h3>
+                            <p>
+                                You can use the PropTypes library to validate the props of your
+                                React components. PropTypes allows you to specify the type and
+                                shape of the props that your component expects. For example, if
+                                you have a component that expects a string prop called "name",
+                                you can use PropTypes to validate that the prop is indeed a
+                                string. If the prop is not a string, PropTypes will issue a
+                                warning in the console.
+                            </p>
+                            <hr className="mb-3" />
 
-                <h5>Anaswer to-2: </h5>
-                <p></p>
-                <hr />
+                            <h3 className="mt-4">
+                                What is the difference between nodeJS and expressJS.
+                            </h3>
+                            <p>
+                                Node.js is a runtime environment for JavaScript that allows you
+                                to run JavaScript code on the server side. Express.js is a web
+                                application framework for Node.js that makes it easier to build
+                                web applications. Node.js provides the runtime environment,
+                                while Express.js provides the framework for building web
+                                applications. Node.js is more low-level, while Express.js
+                                provides more abstractions and features for building web
+                                applications.
+                            </p>
+                            <hr className="mb-3" />
 
-                <h5>Anaswer to-3: </h5>
-                <p></p>
-                <hr />
-
-                <h5>Anaswer to-4: </h5>
-                <p></p>
-                <hr />
-
-                <h5>Anaswer to-5: </h5>
-                <p></p>
-                <hr />
-
+                            <h3 className="mt-4">
+                                What is a custom hook, why will we create a custom hook?
+                            </h3>
+                            <p>
+                                A custom hook is a function that uses React's hook system to
+                                provide some reusable behavior. Custom hooks allow you to
+                                abstract away some complex or repetitive behavior into a simple
+                                function that can be reused across multiple components. You
+                                might create a custom hook if you find yourself duplicating code
+                                across multiple components, or if you want to share some
+                                behavior between components without creating a higher-order
+                                component.
+                            </p>
+                            <Link to="/" className="btn btn-primary mt-4">
+                                Go Back
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/pages/Home/Cart/Cart.jsx
+++ src/pages/Home/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ChefsContext } from '../../../layouts/Main';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 
 const Cart = () => {
 
@@ -13,22 +14,31 @@ const Cart = () => {
         <div>
             <h1>ALL OF OUR CHEFS IS HERE.</h1>
             <p>PLEASE TAP ON A CARD TO KNOW ABOUT OUR CHEFS AND THE SUPER TESY RECIPES</p>
-            {
-                chefs.map(chef =>
-                    // console.log(chef)
-                    <div key={chef.id}>
-                        <img className='w-25' src={chef.picture} alt="" />
-                        <h4>{chef.name}</h4>
-                        <p>Years Of Experience: {chef.years_of_experience}</p>
-                        <p>Number of recipes :{chef.num_recipes}</p>
-                        <h5>Likes:{chef.likes}</h5>
-                    </div>
 
-                )
-            }
-            <Link to="chef_recipes"><button className='btn btn-primary'>View Recipes</button></Link>
+            <Row>
+                {chefs.map(chef =>
+                    <Col key={chef.id} className="m-3">
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={chef.picture} />
+                            <Card.Body>
+                                <Card.Title>{chef.name}</Card.Title>
+                                <Card.Text>
+                                    Years Of Experience: {chef.years_of_experience}
+                                    <br />
+                                    Number of recipes: {chef.num_recipes}
+                                    <br />
+                                    Likes: {chef.likes}
+                                </Card.Text>
+                                <Link to="chef_recipes"><Button variant="primary">View Recipes</Button></Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                )}
+            </Row>
+
         </div>
     );
 };
 
 export default Cart;
+
