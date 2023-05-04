28cbe876f7c9e965c1e0a11ef1e7decfa2cc0bd4
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-05-04 19:37:01 +0600

    chefs data loaded in cart

--- src/pages/Home/Cart/Cart.jsx
+++ src/pages/Home/Cart/Cart.jsx
@@ -1,38 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { ChefsContext } from '../../../layouts/Main';
 
 const Cart = () => {
 
-    // loading data from backend server'
+    // loading data from backend server with context api'
 
-    // const [chefs, setChef] = useState([]);
-
-    // useEffect(() => {
-    //     fetch('http://localhost:5000/catagories')
-    //         .then(res => res.json())
-    //         .then(data => setChef(data));
-
-
-    // }, [])
     const [chefs] = useContext(ChefsContext)
-    console.log(chefs)
-    // const recipes = us
+    // console.log(chefs)
 
     return (
         <div>
             <h1>ALL OF OUR CHEFS IS HERE.</h1>
             <p>PLEASE TAP ON A CARD TO KNOW ABOUT OUR CHEFS AND THE SUPER TESY RECIPES</p>
-            {/* {
-                chefs.map(chef => 
-                    // console.log(chef);
+            {
+                chefs.map(chef =>
+                    // console.log(chef)
                     <div key={chef.id}>
-                        <img src={chef.image} alt="" />
-                        <h1>Name:{chef.name}</h1>
+                        <img className='w-25' src={chef.picture} alt="" />
+                        <h4>{chef.name}</h4>
+                        <p>Years Of Experience: {chef.years_of_experience}</p>
+                        <p>Number of recipes :{chef.num_recipes}</p>
+                        <h5>Likes:{chef.likes}</h5>
                     </div>
 
                 )
-            } */}
+            }
             <Link to="chef_recipes"><button className='btn btn-primary'>View Recipes</button></Link>
         </div>
     );
